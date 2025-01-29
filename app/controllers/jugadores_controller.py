d47939eb8a71f53b8946238727b017b89027c814
Fernando Montero <monterofernandoe@gmail.com>
2025-01-29 20:06:00 -0300

    Jugadores

--- app/controllers/jugadores_controller.py
+++ app/controllers/jugadores_controller.py
@@ -13,17 +13,27 @@ class JugadoresController:
         if missing_fields:
             return {'error': f'Faltan los siguientes campos: {", ".join(missing_fields)}'}, 400
 
-        jugador = Jugadores(**data)
-        Jugadores.create(jugador)
-        return {'mensaje': 'Jugador creado con éxito'}, 200
+        data2 = {'email': data["email"]}
+        response = Jugadores.get(data2)
+        
+        if response is None:
+
+            jugador = Jugadores(**data)
+            Jugadores.create(jugador)
+            return {'mensaje': 'Jugador creado con éxito'}, 200
+        
+        else:
+            return{'mensaje': 'El mail usado ya esta registrado'}, 400
+        
     @classmethod
     def get_all(cls):
-        categories=Jugadores.get_all()
-        return [Jugadores.serialize() for Jugadores in categories],200
+        response=Jugadores.get_all()
+        print (response)
+        return [Jugadores.serialize() for Jugadores in response],200
     @classmethod
     def get (cls):
         data=request.json
-        permitted=('id','name')
+        permitted=('email', 'id')
         if len(data)>1:
             return {'error':'se ha ingresado mas de un dato'},400
         if not list(data)[0] in permitted:
@@ -33,14 +43,14 @@ class JugadoresController:
     @classmethod
     def update(cls):
         data=request.json
-        if not 'id' in data:
-            return {'mensaje':'No se ingreso el id a modificar'},400
+        if not 'email' in data:
+            return {'mensaje':'No se ingreso el mail a modificar'},400
         Jugadores.update(data)
-        return {'mensaje':'Categoria modificada con éxito'},200
+        return {'mensaje':'Jugador modificado con éxito'},200
     @classmethod
     def delete(cls):
         data=request.json
         if not 'id' in data:
             return {'error':'no se ingreso el id a eliminar'},400
         Jugadores.delete(data)
-        return {'mensaje':'Se elimino la categoria con éxito'},200
+        return {'mensaje':'Se elimino ej Jugador con éxito'},200
